f026edabdf484a44e374b7f17cdb741d957c2bfe
Maisha Mir <maisha.mir2014@gmail.com>
2026-06-06 21:56:36 -0700

    implement filter functionality

--- src/components/RecipeFilter/RecipeFilter.jsx
+++ src/components/RecipeFilter/RecipeFilter.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import chevronDown from '/assets/images/icon-chevron-down.svg';
 import "./RecipeFilter.scss";
 
-function RecipeFilter({ choices = [], label = "", toggleButton, isOpen, name = "", setTime, selectedValue }) {
+function RecipeFilter({ choices = [], label = "", toggleButton, isOpen, name = "", setTime, selectedValue, close }) {
+
+    const filterBoxRef = useRef();
+
+    useEffect(() => {
+        const clickHandler = e => {
+            if (!(filterBoxRef.current.contains(e.target))) close()
+        }
+        document.addEventListener("click", clickHandler)
+        return () => document.removeEventListener("click", clickHandler)
+    }, [])
 
     return (
-        <div className="recipeFilter">
+        <div className="recipeFilter" ref={filterBoxRef}>
             <button className="recipeFilter__button" onClick={toggleButton}>{label} <img src={chevronDown} alt="" /></button>
             <div className={`recipeFilter__filters ${isOpen ? "recipeFilter__filters--open" : "recipeFilter__filters--closed"}`} >
 
@@ -13,7 +23,7 @@ function RecipeFilter({ choices = [], label = "", toggleButton, isOpen, name = "
                     {choices.map((choice, index) => {
                         return (
                             <div className="recipeFilter__option">
-                                <input type="radio" id={`${name}-${choice}`} name={name} value={choice} onChange={() => setTime(choice)} checked={choice === selectedValue} />
+                                <input type="radio" id={`${name}-${choice}`} name={name} value={choice} onChange={() => { setTime(choice); toggleButton() }} checked={choice === selectedValue} />
                                 <label htmlFor={`${name}-${choice}`}>{choice} minutes</label>
                             </div>
                         )
--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -25,8 +25,8 @@ function Recipes() {
             </section>
 
             <section className="recipes__filters">
-                <RecipeFilter choices={[0, 5, 10]} label="Max Prep Time" isOpen={isPrepOpen} toggleButton={() => setIsPrepOpen(!isPrepOpen)} name={"prepTime"} setTime={setMaxPrepTime} selectedValue={maxPrepTime} />
-                <RecipeFilter choices={[0, 5, 10, 15, 20]} label="Max Cook Time" isOpen={isCookOpen} toggleButton={() => setIsCookOpen(!isCookOpen)} name={"cookTime"} setTime={setMaxCookTime} selectedValue={maxCookTime}/>
+                <RecipeFilter choices={[0, 5, 10]} label="Max Prep Time" isOpen={isPrepOpen} toggleButton={() => setIsPrepOpen(!isPrepOpen)} name={"prepTime"} setTime={setMaxPrepTime} selectedValue={maxPrepTime} close={() => setIsPrepOpen(false)}/>
+                <RecipeFilter choices={[0, 5, 10, 15, 20]} label="Max Cook Time" isOpen={isCookOpen} toggleButton={() => setIsCookOpen(!isCookOpen)} name={"cookTime"} setTime={setMaxCookTime} selectedValue={maxCookTime} close={() => setIsCookOpen(false)}/>
 
                 <div className="recipes__filters-search-wrapper">
                     <input className="recipes__filters-search" type="text" name="" id="" placeholder='Search by name or ingredient...' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
