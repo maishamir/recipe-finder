03247d1081d5bebd56d075624c4badace4f128e0
Maisha Mir <maisha.mir2014@gmail.com>
2026-06-08 01:08:01 -0700

    style recipe details for desktop

--- src/pages/RecipeDetails/RecipeDetails.jsx
+++ src/pages/RecipeDetails/RecipeDetails.jsx
@@ -17,7 +17,7 @@ function RecipeDetails() {
     function getMoreRecipes(recipeId, recipeData) {
         const recipeIdx = recipeData.findIndex(recipe => recipe.id === parseInt(recipeId));
         const totalRecipes = recipeData.length;
-        return [-2, -1, 1, 2].map(offset => recipeData[(recipeIdx + offset + totalRecipes) % totalRecipes]);
+        return [-1, 1, 2].map(offset => recipeData[(recipeIdx + offset + totalRecipes) % totalRecipes]);
     }
 
     const moreRecipes = getMoreRecipes(recipeId, recipeData);
@@ -28,41 +28,44 @@ function RecipeDetails() {
         <div className="recipeDetails">
             <nav className="recipeDetails__breadcrumb"> <Link to="/recipes" className='recipe-page-link' scroll={false}>Recipes </Link> <span className='separator'>/</span> {title}</nav>
 
-            <picture>
-                <source media="(min-width: 600px)" srcset={image.large} />
-                <img className="recipeDetails__image" src={image.small} alt="" />
-            </picture>
+            <div className="recipeDetails__content">
+                <picture>
+                    <source media="(min-width: 600px)" srcset={image.large} />
+                    <img className="recipeDetails__image" src={image.small} alt="" />
+                </picture>
 
-            <h1 className="recipeDetails__title">{title}</h1>
-            <p className="recipeDetails__overview">{overview}</p>
+                <div className="recipeDetails__details">
+                    <h1 className="recipeDetails__title">{title}</h1>
+                    <p className="recipeDetails__overview">{overview}</p>
 
-            <div className="recipeDetails__stats">
-                <div className="recipeDetails__stats-item"><img src={servingIcon} alt="" />
-                    <p>Servings: {servings}</p></div>
-                <div className="recipeDetails__stats-item"><img src={prepIcon} alt="" />
-                    <p>Prep: {prepMinutes}</p></div>
-                <div className="recipeDetails__stats-item"><img src={cookIcon} alt="" />
-                    <p>Cook: {cookMinutes} minute{(cookMinutes > 1 || cookMinutes === 0) && 's'} </p></div>
+                    <div className="recipeDetails__stats">
+                        <div className="recipeDetails__stats-item"><img src={servingIcon} alt="" />
+                            <p>Servings: {servings}</p></div>
+                        <div className="recipeDetails__stats-item"><img src={prepIcon} alt="" />
+                            <p>Prep: {prepMinutes}</p></div>
+                        <div className="recipeDetails__stats-item"><img src={cookIcon} alt="" />
+                            <p>Cook: {cookMinutes} minute{(cookMinutes > 1 || cookMinutes === 0) && 's'} </p></div>
+                    </div>
+
+                    <div className="recipeDetails__ingredients">
+                        <h3 className="recipeDetails__ingredients-title">Ingredients:</h3>
+                        <ul className="recipeDetails__ingredients-list">
+                            {ingredients.map((ingredient, index) => {
+                                return <li key={index} className="recipeDetails__ingredients-item">{ingredient}</li>
+                            })}
+                        </ul>
+                    </div>
+
+                    <div className="recipeDetails__instructions">
+                        <h3 className="recipeDetails__instructions-title">Instructions:</h3>
+                        <ul className="recipeDetails__instructions-list">
+                            {instructions.map((step, index) => {
+                                return <li key={index} className="recipeDetails__instructions-step">{step}</li>
+                            })}
+                        </ul>
+                    </div>
+                </div>
             </div>
-
-            <div className="recipeDetails__ingredients">
-                <h3 className="recipeDetails__ingredients-title">Ingredients:</h3>
-                <ul className="recipeDetails__ingredients-list">
-                    {ingredients.map((ingredient, index) => {
-                        return <li key={index} className="recipeDetails__ingredients-item">{ingredient}</li>
-                    })}
-                </ul>
-            </div>
-
-            <div className="recipeDetails__instructions">
-                <h3 className="recipeDetails__instructions-title">Instructions:</h3>
-                <ul className="recipeDetails__instructions-list">
-                    {instructions.map((step, index) => {
-                        return <li key={index} className="recipeDetails__instructions-step">{step}</li>
-                    })}
-                </ul>
-            </div>
-
 
             <div className="moreRecipes">
                 <h2 className='moreRecipes__title'>More recipes</h2>
