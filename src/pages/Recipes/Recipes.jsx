9ebc79c13b62c2a66dea75c969e6eeea5465ea3d
Maisha Mir <maisha.mir2014@gmail.com>
2026-06-08 01:01:00 -0700

    style recipes page for desktop

--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -14,19 +14,21 @@ function Recipes() {
     const [searchQuery, setSearchQuery] = useState("");
 
     const filteredRecipes = recipeData.filter(recipe => recipe.title.toLowerCase().includes(searchQuery.toLowerCase())).filter(recipe => maxPrepTime === null || recipe.prepMinutes <= maxPrepTime).filter(recipe => maxCookTime === null || recipe.cookMinutes <= maxCookTime);
-    
+
 
 
     return (
         <div className="recipes">
             <section className="recipes__hero">
-                <h1 className='recipes__hero-title'>Explore our simple, healthy recipes</h1>
-                <p className='recipes__hero-text'>Discover eight quick, whole-food dishes that fit real-life schedules and taste amazing. Use the search  bar to find a recipe by name or ingredient, or simply scroll the list and let something delicious catch your eye.</p>
+                <div className="recipes__hero-content">
+                    <h1 className='recipes__hero-title'>Explore our simple, healthy recipes</h1>
+                    <p className='recipes__hero-text'>Discover eight quick, whole-food dishes that fit real-life schedules and taste amazing. Use the search  bar to find a recipe by name or ingredient, or simply scroll the list and let something delicious catch your eye.</p>
+                </div>
             </section>
 
             <section className="recipes__filters">
-                <RecipeFilter choices={[0, 5, 10]} label="Max Prep Time" isOpen={isPrepOpen} toggleButton={() => setIsPrepOpen(!isPrepOpen)} name={"prepTime"} setTime={setMaxPrepTime} selectedValue={maxPrepTime} close={() => setIsPrepOpen(false)}/>
-                <RecipeFilter choices={[0, 5, 10, 15, 20]} label="Max Cook Time" isOpen={isCookOpen} toggleButton={() => setIsCookOpen(!isCookOpen)} name={"cookTime"} setTime={setMaxCookTime} selectedValue={maxCookTime} close={() => setIsCookOpen(false)}/>
+                <RecipeFilter choices={[0, 5, 10]} label="Max Prep Time" isOpen={isPrepOpen} toggleButton={() => setIsPrepOpen(!isPrepOpen)} name={"prepTime"} setTime={setMaxPrepTime} selectedValue={maxPrepTime} close={() => setIsPrepOpen(false)} />
+                <RecipeFilter choices={[0, 5, 10, 15, 20]} label="Max Cook Time" isOpen={isCookOpen} toggleButton={() => setIsCookOpen(!isCookOpen)} name={"cookTime"} setTime={setMaxCookTime} selectedValue={maxCookTime} close={() => setIsCookOpen(false)} />
 
                 <div className="recipes__filters-search-wrapper">
                     <input className="recipes__filters-search" type="text" name="" id="" placeholder='Search by name or ingredient...' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
