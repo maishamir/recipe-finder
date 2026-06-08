e919ccb9eec36c42a16d05246d146f90cb83babf
Maisha Mir <maisha.mir2014@gmail.com>
2026-06-08 00:22:57 -0700

    style homepage for desktop

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import heroImg from "/assets/images/image-home-hero-small.webp";
+import heroImgLg from "/assets/images/image-home-hero-large.webp";
 import carrotIcon from "/assets/images/icon-whole-food-recipes.svg";
 import boltIcon from "/assets/images/icon-minimum-fuss.svg";
 import searchIcon from "/assets/images/icon-search-in-seconds.svg";
@@ -18,13 +19,20 @@ function Home() {
     return (
         <div className='home'>
             <section className='home__hero'>
-                <h1 className='home__hero-title'><span className="healthy">Healthy</span> meals, zero fuss</h1>
-                <p className='home__hero-subtitle'>Discover eight quick, whole-food recipes that you can cook tonight&mdash;no processed junk, no guesswork.</p>
-                <button className='home__hero-cta' onClick={() => navigate("/recipes")}>Start exploring</button>
+                <div className="home__hero-text">
+                    <h1 className='home__hero-title'><span className="healthy">Healthy</span> meals, zero fuss</h1>
+                    <p className='home__hero-subtitle home__hero-subtitle--mob'>Discover eight quick, whole-food recipes that you can cook tonight&mdash;no processed junk, no guesswork.</p>
+                    <p className='home__hero-subtitle home__hero-subtitle--dk'>Discover eight quick, whole-food recipes that you can cook tonight<br />&mdash;no processed junk, no guesswork.</p>
+                    <button className='home__hero-cta' onClick={() => navigate("/recipes")}>Start exploring</button>
+                </div>
 
                 <div className="home__hero-img">
                     <img src={squiggle} className="squiggle" alt="" />
-                    <img src={heroImg} className='woman' alt="woman chopping vegetables and smiling" />
+
+                    <picture>
+                        <source media="(min-width: 1200px)" srcset={heroImgLg} />
+                        <img src={heroImg} className='woman' alt="woman chopping vegetables and smiling" />
+                    </picture>
                 </div>
 
             </section>
@@ -64,11 +72,12 @@ function Home() {
             </section>
 
             <section className="home__real-life">
-                <h2 className='home__real-life-title'>Built for real life</h2>
-                <div className="home__real-life-desc">
-                    <p>Cooking shouldn't be complicated. These recipes come in under <span className='half-hour'>30 minutes</span> of active time, fit busy schedules, and taste good enough to repeat.</p>
-                    <p>Whether you're new to the kitchen or just need fresh ideas, we've got you covered.</p>
-                </div>
+                <div className="home__real-life-txt">
+                    <h2 className='home__real-life-title'>Built for real life</h2>
+                    <div className="home__real-life-desc">
+                        <p>Cooking shouldn't be complicated. These recipes come in under <span className='half-hour'>30 minutes</span> of active time, fit busy schedules, and taste good enough to repeat.</p>
+                        <p>Whether you're new to the kitchen or just need fresh ideas, we've got you covered.</p>
+                    </div></div>
 
 
                 <picture>
